261338d628c9490dde7348f7d1ce50630828cbad
Matt Ridenour <matt@goodguide.com>
2014-06-20 17:12:33 -0700

    Restore testing framework

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '55cda8f2857cb05471e8a480e37f792398fe59f605e47ffced883b5fd36fbc1cbfaef1fbdb40981693f3e6a44f5743b3b7bff072bca4894079e3324da4266bb4'

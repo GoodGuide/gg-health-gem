fc1d34031b42fbc98235207b22f18cc9b8d7b502
Ryan Aipperspach <ryan@goodguide.com>
2014-10-27 17:42:51 -0700

    Expose maintenance mode helpers [#81324808]

--- app/helpers/goodguide/health/maintenance_helper.rb
+++ app/helpers/goodguide/health/maintenance_helper.rb
@@ -1,12 +1,6 @@
 module Goodguide
   module Health
     module MaintenanceHelper
-      def redirect_if_maintenance_mode(path)
-        if maintenance? && request.path != path && request.path !~ /health/
-          redirect_to path
-        end
-      end
-
       def maintenance?
         Goodguide::Health::Maintenance.instance.enabled?
       end
